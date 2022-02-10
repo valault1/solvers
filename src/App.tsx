291778a0ae87e0b950513c1b595651661fa15991
Val Ault <valault1@gmail.com>
2022-02-10 11:07:27 -0700

    Committing from new dir

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ export const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to the app!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
