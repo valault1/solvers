eb1a224f8ab33c45ee5272020ee87cc47c90e698
Val Ault <valault1@gmail.com>
2022-08-15 15:11:11 -0400

    started to solve rummikub

--- src/domains/Rummikub/getSolutions.ts
+++ src/domains/Rummikub/getSolutions.ts
@@ -1,14 +1,45 @@
-import { TileData } from "domains/Rummikub/sharedTypes";
+import { CurrentBoard } from "domains/Rummikub/components/CurrentBoard";
+import { TileData as Tile } from "domains/Rummikub/sharedTypes";
 
-export const getPossibleMoves = (
-  board: TileData[][],
-  yourTiles: TileData[]
-) => {
-  const cleanedBoard = cleanBoard(board);
-  console.log({ board: cleanedBoard });
+export type GuessState = {
+  possibleMoves: Tile[][];
+  currentBoard: Tile[][];
+  currentTiles: Tile[];
+  tileGuessingOn: Tile;
+  // id of key to access last guessState
+  stateBeforeLastGuess: null | string;
+  guessTaken: number;
+  id?: string;
 };
 
-const cleanBoard = (board: TileData[][]) => {
+const getPossibleMoves = (
+  currentBoard: Tile[][],
+  currentTiles: Tile[],
+  tileGuessingOn: Tile
+): Tile[][] => {
+  return [];
+};
+
+// Assumes there are possible moves
+const getNextGuessState = (lastGuessState: GuessState): GuessState => {
+  const lastGuessTaken =
+    lastGuessState.possibleMoves[lastGuessState.guessTaken];
+  const nextBoard = [...lastGuessState.currentBoard, lastGuessTaken];
+  const nextTiles = lastGuessState.currentTiles.filter(
+    (tile) => lastGuessTaken.find((t) => t.id === tile.id) === undefined
+  );
+  const nextTileGuessingOn = nextTiles[0];
+  return {
+    possibleMoves: getPossibleMoves(nextBoard, nextTiles, nextTileGuessingOn),
+    currentBoard: nextBoard,
+    currentTiles: nextTiles,
+    tileGuessingOn: nextTileGuessingOn,
+    stateBeforeLastGuess: lastGuessState.id,
+    guessTaken: 0,
+  };
+};
+
+const cleanBoard = (board: Tile[][]) => {
   var cleanedBoard = [];
   for (let row of board) {
     cleanedBoard.push(row);
--- src/domains/Rummikub/sharedTypes.ts
+++ src/domains/Rummikub/sharedTypes.ts
@@ -3,4 +3,5 @@ export type Color = "red" | "orange" | "black" | "blue";
 export type TileData = {
   number: number;
   color: Color;
+  id?: string;
 };
