6447b646d0860a65fb99dfe8cfa9d5e73e8bf133
Val Ault <vault@brex.com>
2024-06-25 14:08:59 -0600

    added 13x13 seeds

---
+++ src/domains/Queens/boards/seeds12.v3.ts
@@ -0,0 +1,10 @@
+export const boardSize = 12;
+export const seeds = [];
+export const timesTakenSeconds = [51.34];
+export const boardsGenerated = [20000];
+export const numSeeds = 0;
+export const totalTimeTaken = 51.34;
+export const totalBoardsGenerated = 20000;
+export const averageTimeTakenPerSeed = null;
+export const averageBoardsGeneratedPerSeed = null;
+export const averageTimePerAttemptMs = 2.567;
