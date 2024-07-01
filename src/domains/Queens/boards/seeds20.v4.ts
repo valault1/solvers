efda57393d1e544e1626956070192b6d40e6bfd4
Val Ault <valault1@gmail.com>
2024-07-01 00:16:35 -0600

    added seeds

--- src/domains/Queens/boards/seeds20.v4.ts
+++ src/domains/Queens/boards/seeds20.v4.ts
@@ -1,11 +1,11 @@
 // @ts-nocheck
 export const boardSize = 20;
-export const seeds = [2282795,318529];
-export const timesTakenSeconds = [19496.648999999998,3607.7560000000003,61140.610999999975];
-export const boardsGenerated = [175831,40000,590000];
-export const numSeeds = 2;
-export const totalTimeTaken = 84245.01599999997;
-export const totalBoardsGenerated = 805831;
-export const averageTimeTakenPerSeed = 42122.50799999999;
-export const averageBoardsGeneratedPerSeed = 402915.5;
-export const averageTimePerAttemptMs = 104.5442729306765;
+export const seeds = [2282795,318529,318529,2282795,7738959];
+export const timesTakenSeconds = [19496.648999999998,3607.7560000000003,81079.55999999994,17130.275000000005,69696.87599999999,20830.467999999997];
+export const boardsGenerated = [175831,40000,1404949,576103,1433165,610000];
+export const numSeeds = 5;
+export const totalTimeTaken = 211841.58399999992;
+export const totalBoardsGenerated = 4240048;
+export const averageTimeTakenPerSeed = 42368.316799999986;
+export const averageBoardsGeneratedPerSeed = 848009.6;
+export const averageTimePerAttemptMs = 49.96207212748533;
