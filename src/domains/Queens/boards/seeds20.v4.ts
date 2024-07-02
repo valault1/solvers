eca1eed1ee3b06669c9cfe4e8574693b98c6edd6
Val Ault <valault1@gmail.com>
2024-07-02 10:13:43 -0600

    found 20x20 seeds

--- src/domains/Queens/boards/seeds20.v4.ts
+++ src/domains/Queens/boards/seeds20.v4.ts
@@ -1,11 +1,11 @@
 // @ts-nocheck
 export const boardSize = 20;
-export const seeds = [2282795,318529,318529,2282795,7738959];
-export const timesTakenSeconds = [19496.648999999998,3607.7560000000003,81079.55999999994,17130.275000000005,69696.87599999999,20830.467999999997];
-export const boardsGenerated = [175831,40000,1404949,576103,1433165,610000];
-export const numSeeds = 5;
-export const totalTimeTaken = 211841.58399999992;
-export const totalBoardsGenerated = 4240048;
-export const averageTimeTakenPerSeed = 42368.316799999986;
-export const averageBoardsGeneratedPerSeed = 848009.6;
-export const averageTimePerAttemptMs = 49.96207212748533;
+export const seeds = [2282795,318529,318529,2282795,7738959,9952369,9952369,8299653];
+export const timesTakenSeconds = [19496.648999999998,3607.7560000000003,81079.55999999994,17130.275000000005,69696.87599999999,34739.17399999999,43270.19900000001,22673.499000000003,41476.372];
+export const boardsGenerated = [175831,40000,1404949,576103,1433165,1015927,740123,372385,650000];
+export const numSeeds = 8;
+export const totalTimeTaken = 333170.36;
+export const totalBoardsGenerated = 6408483;
+export const averageTimeTakenPerSeed = 41646.295;
+export const averageBoardsGeneratedPerSeed = 801060.375;
+export const averageTimePerAttemptMs = 51.98895900948789;
