15f48847f8db071b8f7f7830c7ffaf7b25d45398
Val Ault <vault@brex.com>
2024-07-01 23:47:53 -0600

    added first version of ios app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'solvers.valault.com',
+  appName: 'solvers',
+  webDir: 'build'
+};
+
+export default config;
