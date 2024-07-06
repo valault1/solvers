53767cbd1df5640284ee6f8d2b7c11b3fa3d211c
Val Ault <valault1@gmail.com>
2024-07-06 11:05:34 -0700

    commit from lubuntu

---
+++ src/domains/Queens/boards/seeds20.v5.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+export const boardSize = 20;
+export const seeds = [318529];
+export const timesTakenSeconds = [13881.937999999996,48664.19799999999];
+export const boardsGenerated = [318529,1100000];
+export const numSeeds = 1;
+export const lastSeedTried = 1418529;
+export const totalTimeTaken = 62546.135999999984;
+export const totalBoardsGenerated = 1418529;
+export const averageTimeTakenPerSeed = 62546.135999999984;
+export const averageBoardsGeneratedPerSeed = 1418529;
+export const averageTimePerAttemptMs = 44.09225049329269;
