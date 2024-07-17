752a98f88004f1738597a846d1cd12b29d957901
Val Ault <valault1@gmail.com>
2024-07-16 18:08:00 -0600

    create allSeeds file

---
+++ scripts/boards/seeds16.v4.ts
@@ -0,0 +1,11 @@
+// @ts-nocheck
+export const boardSize = 16;
+export const seeds = [158336];
+export const timesTakenSeconds = [38.33,60.838];
+export const boardsGenerated = [6366,10000];
+export const numSeeds = 1;
+export const totalTimeTaken = 99.168;
+export const totalBoardsGenerated = 16366;
+export const averageTimeTakenPerSeed = 99.168;
+export const averageBoardsGeneratedPerSeed = 16366;
+export const averageTimePerAttemptMs = 6.059391421239154;
--- scripts/consolidateSeeds.ts
+++ scripts/consolidateSeeds.ts
@@ -1,1 +1,14 @@
 // Ok, we have a ton of seeds. We need to consolidate them and have a way to add them to different packs and stuff.
+import boardGenerator from "../src/domains/Queens/helpers/boardGenerators/generateNewBoard.test";
+import fs from "fs";
+
+const allSeeds =
+  //@ts-ignore
+  boardGenerator.allSeeds;
+
+const filePath = "./allSeeds.ts";
+
+allSeeds["12"] = allSeeds["12"].slice(0, 500);
+
+const content = `export const allSeeds = ${JSON.stringify(allSeeds)}`;
+fs.writeFileSync(filePath, content);
