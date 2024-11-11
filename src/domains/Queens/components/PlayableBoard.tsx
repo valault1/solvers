0ee75ef9cc65a61815db3b2b6dad6b2280206469
Val Ault <valault1@gmail.com>
2024-11-10 17:26:38 -0700

    fix undo button

--- src/domains/Queens/components/PlayableBoard.tsx
+++ src/domains/Queens/components/PlayableBoard.tsx
@@ -93,7 +93,7 @@ export const PlayableBoard = ({
         </Stack>
       </Stack>
     ),
-    [hasWon]
+    [clearBoard, hasWon, runRotateBoard, solveBoard, undoLastMove]
   );
 
   if (!board?.length) return null;
