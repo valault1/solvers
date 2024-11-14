9433703a5c33fcc6bc329edc0989897c2fb3b0e6
Val Ault <vault@brex.com>
2024-11-14 16:41:56 -0700

    tweaked the fetching script

--- scripts/lds-ai/data-scraping/getConferenceTalkUrls.cjs
+++ scripts/lds-ai/data-scraping/getConferenceTalkUrls.cjs
@@ -4,10 +4,26 @@ const CONFERENCE_TALKS_URL =
   "https://www.churchofjesuschrist.org/study/general-conference?lang=eng";
 
 //TODO: Make this path relative
-const BASE_FILE_PATH = "/Users/vault/src/solvers/scripts/conference_talks/";
-
+const BASE_FILE_PATH = `${__dirname}/`;
 const BASE_URL = "https://www.churchofjesuschrist.org";
 const CONFERENCE_TALK_URLS_FILE = "conferenceTalkUrls.txt";
+
+const SEPARATOR = "----- NEW TALK -----\n";
+const CONFERENCE_TALKS_TEXT_FILE = "conferenceTalkTexts.txt";
+const FAILED_CONFERENCE_TALK_URLS = "failedUrls.txt";
+const stripHtml = (htmlText) => {
+  return htmlText.replace(/<[^>]+>/g, "");
+};
+
+const writeFile = async (path, content) => {
+  await fs.writeFile(path, content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      // file written successfully
+    }
+  });
+};
 
 function getAllMatches(string, regex) {
   const matches = [];
@@ -24,12 +40,12 @@ function getAllMatches(string, regex) {
 }
 
 // note: Only month `10` or `4` will probably work.
-const getUrl = (year, month) => {
+const getConferenceUrl = (year, month) => {
   return `https://www.churchofjesuschrist.org/study/general-conference/${year}/${month}?lang=eng`;
 };
-const fetchUrl = async (year, month) => {
+const fetchUrl = async (url) => {
   try {
-    const response = await fetch(getUrl(year, month));
+    const response = await fetch(url);
     return await response.text();
   } catch (error) {
     console.error("There has been a problem with your fetch operation:", error);
@@ -40,7 +56,7 @@ const fetchUrl = async (year, month) => {
 async function getTalkUrls(year, month) {
   // once we get the html of a conference page, we need to find all the talks within that.
   // <script>window.__INITIAL_STATE__=
-  const text = await fetchUrl(year, month);
+  const text = await fetchUrl(getConferenceUrl(year, month));
   const encodedText = text
     .split('<script>window.__INITIAL_STATE__="')[1]
     .split('";</script>')[0];
@@ -62,7 +78,7 @@ async function getTalkUrls(year, month) {
   );
   if (!links.length) {
     console.log("No talks found for: ");
-    console.log({ year, month });
+    console.log({ year, month, url: getConferenceUrl(year, month), body });
   }
   return links.map((link) => `${BASE_URL}/${link}`);
   // now, we can get the urls, by looking for the pattern:
@@ -72,31 +88,72 @@ async function getTalkUrls(year, month) {
 }
 
 const getAllTalkUrls = async () => {
+  const startTime = new Date();
   const START_YEAR = 1971;
   const END_YEAR = new Date().getFullYear();
   const allLinks = [];
+
+  const allUrlPromises = [];
   for (let year = START_YEAR; year <= END_YEAR; year++) {
     console.log("getting talks from " + year);
-    const aprilUrls = await getTalkUrls(year, "04");
-    allLinks.push(...aprilUrls);
-    allLinks.push(...(await getTalkUrls(year, "10")));
+    allUrlPromises.push(getTalkUrls(year, "04"));
+    allUrlPromises.push(getTalkUrls(year, "10"));
+  }
+  const allUrlLists = await Promise.all(allUrlPromises);
+  for (let urls of allUrlLists) {
+    allLinks.push(...urls);
   }
   // remove links to sessions
   const links = allLinks.filter((link) => {
     return !link.includes("session");
   });
-  for (let idx in links) console.log(`${idx} - ${links[idx]}`);
-  const content = links.join("\n");
-  fs.writeFile(
-    `${BASE_FILE_PATH}${CONFERENCE_TALK_URLS_FILE}`,
-    content,
-    (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // file written successfully
-      }
-    }
+  //for (let idx in links) console.log(`${idx} - ${links[idx]}`);
+  await fetchContentFromUrlsAndWriteToFile(links);
+  console.log(
+    `time to query all talks: ${(new Date() - startTime) / 1000} seconds`
+  );
+};
+
+const processUrl = async (url, failedUrls) => {
+  const text = await fetchUrl(url);
+  const encodedText = text
+    ?.split('<script>window.__INITIAL_STATE__="')[1]
+    ?.split('";</script>')[0];
+  if (!encodedText) {
+    console.log("url didn't work: " + url);
+    failedUrls.push(url);
+    return undefined;
+  }
+  const decodedText = atob(encodedText);
+  const obj = JSON.parse(decodedText);
+  const uri = Object.keys(obj.reader.contentStore)[0];
+  const content = obj.reader.contentStore[uri].content;
+  const body = content.body;
+  const title = content.head["page-meta-social"].pageMeta.title;
+
+  const year = uri.split("/")[3];
+  const month = uri.split("/")[4];
+  const fileMetadata = { title, year, month };
+  const plainText = stripHtml(body);
+  return `${SEPARATOR}${JSON.stringify(fileMetadata)}\n${plainText}`;
+};
+
+const fetchContentFromUrlsAndWriteToFile = async (urls) => {
+  const promises = [];
+  const failedUrls = [];
+  for (let idx in urls) {
+    const url = urls[idx];
+    console.log("processing URL #" + idx);
+    promises.push(processUrl(url, failedUrls));
+  }
+  const values = await Promise.all(promises);
+  await writeFile(
+    `${BASE_FILE_PATH}${CONFERENCE_TALKS_TEXT_FILE}`,
+    values.join("\n")
+  );
+  await writeFile(
+    `${BASE_FILE_PATH}${FAILED_CONFERENCE_TALK_URLS}`,
+    failedUrls.join("\n")
   );
 };
 
