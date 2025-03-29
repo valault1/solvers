e778d4b32732753f6f6b5f046bac5cec3493ea04
Val Ault <valault1@gmail.com>
2025-03-28 17:16:54 -0700

    edit script

--- backend/remote_scripts/deploy.sh
+++ backend/remote_scripts/deploy.sh
@@ -35,7 +35,7 @@ for service_id in $services; do
   service_name=$(docker inspect --format '{{.Name}}' $service_id)
   $status="$(docker inspect --format='{{json .State.Health.Status}}' $service_id)"
   echo "current status of $service_name: $status"
-  until [ $status = '"healthy"' ]; do
+  until [ "$status" = '"healthy"' ]; do
     echo "Waiting for $service_name to become healthy - $((tries_left--)) tries left"
     sleep 3
   done
