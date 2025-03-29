d7a5d23c4b67f3af69da4ec36e55252422b0ec14
Val Ault <valault1@gmail.com>
2025-03-28 17:22:08 -0700

    fix decrement

--- backend/remote_scripts/deploy.sh
+++ backend/remote_scripts/deploy.sh
@@ -36,7 +36,8 @@ for service_id in $services; do
   status="$(docker inspect --format='{{json .State.Health.Status}}' $service_id)"
   echo "current status of $service_name: $status"
   until [ "$status" = '"healthy"' ]; do
-    echo "Waiting for $service_name to become healthy - $((tries_left--)) tries left"
+    echo "Waiting for $service_name to become healthy - $tries_left tries left"
+    tries_left=$((tries_left - 1))
     sleep 3
   done
   echo "$service_name is healthy"
